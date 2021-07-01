8165f496b3c4c6728fa352eae9301c97a1f31058
Wu-Hui <wu.hui.github@gmail.com>
2021-07-01 11:37:19 -0400

    Trying android

--- shell.nix
+++ shell.nix
@@ -1,9 +1,35 @@
-with import <nixpkgs> { };
+with import <nixpkgs> { config.android_sdk.accept_license = true; };
 
 let
   pythonEnv = python3.withPackages (ps: [ ps.absl-py ps.protobuf ps.attrs ]);
   xcodeenv = import ./nix/xcodeenv.nix { inherit stdenv; } { };
-in mkShell {
+  # android = androidenv.androidPkgs_9_0;
+    android = androidenv.composeAndroidPackages {
+      toolsVersion = "26.1.1";
+      buildToolsVersions = [ "28.0.3" ];
+      includeEmulator = true;
+      emulatorVersion = "30.6.3";
+      platformVersions = [ "28" ];
+      includeSources = false;
+      includeSystemImages = false;
+      systemImageTypes = [ "google_apis_playstore" ];
+      abiVersions = [ "x86_64" "armeabi-v7a" "arm64-v8a" ];
+      cmakeVersions = [ "3.10.2" ];
+      includeNDK = true;
+      ndkVersions = ["17.2.4988734"];
+      useGoogleAPIs = false;
+      useGoogleTVAddOns = false;
+      includeExtras = [
+        "extras;google;gcm"
+      ];
+    };
+in mkShell rec {
+  ANDROID_SDK_ROOT = "${android.androidsdk}/libexec/android-sdk";
+  ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+  ANDROID_HOME = "${ANDROID_SDK_ROOT}";
+  ANDROID_NDK_HOME = "${ANDROID_NDK_ROOT}";
+  # GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android.androidsdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
+
   packages = [
     pythonEnv
     cmake
@@ -12,6 +38,7 @@ in mkShell {
     xcodeenv
     cocoapods
     git
+    # android-studio
 
     nixfmt
     which
@@ -19,9 +46,14 @@ in mkShell {
     neovim
     emacs
   ];
+  buildInputs = [
+    jdk11
+    # gradle
+    android.androidsdk
+  ];
   nativeBuildInputs = [ boringssl ];
   # TODO(wuandy): this is probably a setup error on my machine, not required for other
-  # people. We should fix the issue properly ana remove this hack.
+  # people. We should fix the issue properly and remove this hack.
   shellHook = ''
     unset CMAKE_FRAMEWORK_PATH
   '';
