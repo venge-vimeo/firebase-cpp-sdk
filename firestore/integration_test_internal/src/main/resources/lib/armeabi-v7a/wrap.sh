4086b8c7372a270b75d3da2b260e991c4ac593e8
Denver Coneybeare <dconeybe@google.com>
2021-07-23 14:13:29 -0400

    DO NOT SUBMIT: Add fast_unwind_on_malloc=0 to ASAN_OPTIONS in wrap.sh

--- firestore/integration_test_internal/src/main/resources/lib/armeabi-v7a/wrap.sh
+++ firestore/integration_test_internal/src/main/resources/lib/armeabi-v7a/wrap.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 HERE="$(cd "$(dirname "$0")" && pwd)"
-export ASAN_OPTIONS=log_to_syslog=false,allow_user_segv_handler=1,color=never
+export ASAN_OPTIONS=log_to_syslog=false,allow_user_segv_handler=1,color=never,fast_unwind_on_malloc=0
 ASAN_LIB=$(ls $HERE/libclang_rt.asan-*-android.so)
 if [ -f "$HERE/libc++_shared.so" ]; then
     # Workaround for https://github.com/android-ndk/ndk/issues/988.
